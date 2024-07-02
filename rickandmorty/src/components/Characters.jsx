23ee86704ae52ed920b3b5e106071a6daf61ffef
Luiz Carlos <luizcarlosdk@hotmail.com>
2024-07-02 17:01:53 -0300

    Open modal when the character card is clicked
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- rickandmorty/src/components/Characters.jsx
+++ rickandmorty/src/components/Characters.jsx
@@ -3,6 +3,14 @@ import { useEffect, useState } from "react";
 
 function Characters() {
   const [characters, setCharacters] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [modalCharacter, setModalCharacter] = useState({});
+
+  const HandleClick = (character) => {
+    setModalCharacter(character);
+    setModal(!modal);
+  };
+
   useEffect(() => {
     const fetchCharacters = async () => {
       const query = `query { characters { results { name image status episode {id }} } } `;
@@ -22,19 +30,46 @@ function Characters() {
 
         .catch((error) => {
           window.alert(error);
+          console.log(error)
         });
     };
 
     fetchCharacters();
-  }, []);
+  });
 
   return (
     <div className="charactersList">
+      {modal && (
+        <div className="modal">
+          <div className="modalContent">
+            <img
+              className="cardImage"
+              src={modalCharacter.image}
+              alt={modalCharacter.name}
+            />
+            <h2>{modalCharacter.name}</h2>
+            <p>
+              <b>Episodes</b>:{" "}
+              {modalCharacter.episode.map((episode) => episode.id).join(", ")}
+            </p>
+            <button className="closeModal" onClick={HandleClick}>
+              Retornar a listagem
+            </button>
+          </div>
+        </div>
+      )}
+
       {characters.map((character) => (
-        <div className="card">
-          <img className="cardImage" src={character.image} alt={character.name} />
+        <div className="card" onClick={() => HandleClick(character)}>
+          <img
+            className="cardImage"
+            src={character.image}
+            alt={character.name}
+          />
           <h2>{character.name}</h2>
-          <p>Status: {character.status}</p>
+          <p>
+            <b>Status</b>: {character.status}
+          </p>
         </div>
       ))}
     </div>
